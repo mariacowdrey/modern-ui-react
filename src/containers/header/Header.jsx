49bdaad679abe5544eef9419e467475209f79ba1
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-12-24 13:57:51 -0800

    update header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+
 
 function Header() {
     return (
-        <div>
-            Header
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">
+                    Let's build something amazing with GPT-3 OpenAI
+                </h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo ea, veritatis eveniet, fugiat atque similique dolores omnis architecto placeat error culpa, praesentium deleniti cum facilis a libero dolore quam molestiae!</p>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your email address"></input>
+                    <button type="button">Get Started</button>
+                </div>
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="people"/>
+                    <p>1,600 people requested access to visit in the last 24 hours</p>
+                </div>
+            </div>
+            <div className="gpt3__header-image">
+                    <img src={ai} alt="ai"/>
+            </div>
         </div>
     )
 }
